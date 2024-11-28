20ddb4b6395333e977c098c12dd156733487cbe9
hala ghattas <halaghattas350000@gmail.com>
2024-11-27 23:38:47 -0800

    update _ DFS R

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-import heapq
+
 class Game:
     def __init__(self, rows, cols, board_data):
         self.rows = rows
@@ -26,122 +26,52 @@ class Game:
 
     def simulate_move(self, _row, _col):
         new_states = []
-
         for idx, player in enumerate(self.players):
             if self.reach_goal[idx]:
                 continue
-
             current_row, current_col = player["position"]
-
             while True:
                 next_row = current_row + _row
                 next_col = current_col + _col
-
                 if not self.is_valid_move(next_row, next_col, idx):
                     break
-
                 current_row, current_col = next_row, next_col
-
                 if (current_row, current_col) == self.goals[idx]["position"]:
                     self.reach_goal[idx] = True
                     break
-
             if (current_row, current_col) != player["position"]:
                 cloned_game = deepcopy(self)
                 cloned_game.players[idx]["position"] = (current_row, current_col)
                 new_states.append(cloned_game)
-
         return new_states
 
     def solve_with_dfs(self):
-        initial_state = deepcopy(self)
-        stack = [(initial_state, [])]
         visited = set()
-        nodes_visited = 0
+        nodes_visited = [0]
+        path = []
 
-        while stack:
-            current_game, path = stack.pop()
-            nodes_visited += 1
-
+        def dfs(current_game):
+            nodes_visited[0] += 1
             if all(current_game.reach_goal):
-                return path + [current_game], nodes_visited
-
+                return True
             state_hash = self.get_state_hash(current_game)
             if state_hash in visited:
-                continue
+                return False
             visited.add(state_hash)
-
             for move_row, move_col in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 new_states = current_game.simulate_move(move_row, move_col)
                 for new_state in new_states:
-                    stack.append((new_state, path + [current_game]))
+                    path.append(new_state)
+                    if dfs(new_state):
+                        return True
+                    path.pop()
+            return False
 
-        return None, nodes_visited
-
-
-    def solve_with_ucs(self):
         initial_state = deepcopy(self)
-        #the cost is 1 
-        priority_queue = [(1, initial_state, [])]  
-        visited = set()
-        nodes_visited = 0
+        if dfs(initial_state):
+            return path, nodes_visited[0]
+        return None, nodes_visited[0]
 
-        while priority_queue:
-            cost, current_game, path = heapq.heappop(priority_queue)
-            nodes_visited += 1
-
-            if all(current_game.reach_goal):
-                return path + [current_game], nodes_visited
-
-            state_hash = self.get_state_hash(current_game) 
-            if state_hash in visited:
-                continue
-            visited.add(state_hash)
-
-            for move_row, move_col in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                new_states = current_game.simulate_move(move_row, move_col)
-                for new_state in new_states:
-                    new_cost = cost + 1  
-                    heapq.heappush(priority_queue, (new_cost, new_state, path + [current_game]))
-
-        return None, nodes_visited
-    
-    def get_state_hash(self,game):
+    def get_state_hash(self, game):
         players_positions = tuple(player["position"] for player in game.players)
         return (players_positions, tuple(game.reach_goal))
-    
-    
-    
-    def dfs_r(self):
-        def dfs(current_game, path, visited):
-            nonlocal nodes_visited
-
-            nodes_visited += 1
-
-    
-            if all(current_game.reach_goal):
-                return path + [current_game]
-
-            state_hash = current_game.get_state_hash()
-            if state_hash in visited:
-                return None
-            visited.add(state_hash)
-
-        
-            for move_row, move_col in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                new_states = current_game.simulate_move(move_row, move_col)
-                for new_state in new_states:
-                    result = dfs(new_state, path + [current_game], visited)
-                    if result is not None:
-                        return result
-
-            visited.remove(state_hash) 
-            return None
-
-        initial_state = deepcopy(self)
-        nodes_visited = 0
-        visited = set()
-    
-        return dfs(initial_state, [], visited), nodes_visited
-
- 
--- main.py
+++ main.py
@@ -3,4 +3,3 @@ from board import GameUI
 if __name__ == "__main__":
     game_ui = GameUI(15, 15)
     game_ui.run()
-    
